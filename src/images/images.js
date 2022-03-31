cf31485379da5318665b1a485997086f0ded37a8
salva-olacua <salvaolacua@gmail.com>
2022-03-31 20:27:05 -0300

    remove warnings

--- src/images/images.js
+++ src/images/images.js
@@ -13,11 +13,6 @@ import pinsir from './megaPinsir.jpg';
 import rayquaza from './megaRayquaza.jpg';
 import salamence from './megaSalamence.jpg';
 import venusaur from './megaVenusaur.jpg';
-import one from './1.jpg';
-import two from './2.jpg';
-import three from './3.jpg';
-import four from './4.jpg';
-import five from './5.jpg';
 
 const images = [
   gengar,
